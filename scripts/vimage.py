340d82c90eeb444a0e74fa1a94e8cfa00ca0bc0a
Christopher Bruns <cmbruns@rotatingpenguin.com>
2021-12-15 17:39:48 -0800

    Add hack to get pyinstaller working with opengl

--- scripts/vimage.py
+++ scripts/vimage.py
@@ -1,3 +1,18 @@
+# Hack to make it work with pyinstaller
+try:
+    from OpenGL.platform import win32  # required
+except AttributeError:
+    pass
+
+try:
+    from OpenGL.arrays import (
+        numpymodule,  # required
+        ctypesarrays,  # required
+        strings,  # required
+    )
+except AttributeError:
+    pass
+
 from vmg import VimageApp
 
 VimageApp()
