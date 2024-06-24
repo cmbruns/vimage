f4e7da11d87ca66f63acd86cd0d27912407579c9
Christopher Bruns <cmbruns@rotatingpenguin.com>
2024-06-24 09:52:43 -0700

    Clean up create_git_hash_txt.py

--- scripts/create_git_hash_txt.py
+++ scripts/create_git_hash_txt.py
@@ -2,6 +2,9 @@ import subprocess
 
 subprocess.run([*"git update-index -q --refresh".split()])
 abb_hash = subprocess.check_output(["git", "describe", "--always", "--dirty", "--broken"], cwd="..")
-print(f"vimage_git_hash = {abb_hash.decode().strip()}")
+line = f"vimage_git_hash = {abb_hash.decode().strip()}"
+# Print to output
+print(line)
+# Write to file where the app can access it
 with open("../vmg/git_hash.txt", "w") as fh:
-    print(f"vimage_git_hash = {abb_hash.decode().strip()}", file=fh)
+    print(line, file=fh)
