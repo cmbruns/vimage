347f32558a362645351e0c055c8fe725bbf615be
Christopher Bruns <cmbruns@rotatingpenguin.com>
2026-06-28 18:36:39 -0700

    Make sure linux vimage is installed with executable permission. Create a symlink if needed.

--- CMake/resources/debian/postinst.in.sh
+++ CMake/resources/debian/postinst.in.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+set -e
 
 update-mime-database /usr/share/mime
+
+# Create symlink if missing
+if [ ! -e /usr/local/bin/vimage ]; then
+    ln -s /usr/local/vimage/vimage /usr/local/bin/vimage
+fi
