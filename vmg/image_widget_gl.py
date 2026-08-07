0e6a3bb00f583826242fdfa468f10d9f9ab046e4
Christopher Bruns <cmbruns@rotatingpenguin.com>
2026-08-07 16:27:09 -0700

    Add "Reset view" context action.

--- vmg/image_widget_gl.py
+++ vmg/image_widget_gl.py
@@ -235,14 +235,24 @@ class ImageWidgetGL(QtOpenGLWidgets.QOpenGLWidget):
         if self.view_state.center_on_point(qpoint):
             self.update()
 
+    def reset_view(self):
+        self.view_state.reset()
+        self.update()
+
     @QtCore.Slot(QPoint)
     def show_context_menu(self, qpoint: QPoint):
         menu = QtWidgets.QMenu("Context menu", parent=self)
         menu.addSeparator()
         if self.image is not None:
+            #
             center_point_action = QtGui.QAction(text="Center on this point")
             center_point_action.triggered.connect(lambda: self.center_on_point(qpoint))  # noqa
             menu.addAction(center_point_action)
+            #
+            reset_view_action = QtGui.QAction(text="Reset view")
+            reset_view_action.triggered.connect(lambda: self.reset_view())  # noqa
+            menu.addAction(reset_view_action)
+            #
             for action in self.view_state.context_menu_actions(qpoint):
                 menu.addAction(action)
         menu.addSeparator()
